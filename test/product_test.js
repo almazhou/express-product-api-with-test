eaa85c6e9a986be721fa5271a2259d0b7d5c54b8
almazhou <zx_new@126.com>
2014-07-08 22:09:25 +0800

    refactor code

--- test/product_test.js
+++ test/product_test.js
@@ -1,16 +1,16 @@
 var request = require('supertest'),
 should = require('should');
-var product = require('../model/products');
 var app = require("../server");
 var mongoose = require("mongoose");
-var product_schema = product.getSchema();
-var ProductModel = mongoose.model("Product",product_schema);
+var mockgoose = require("mockgoose");
+mockgoose(mongoose);
+var ProductModel = mongoose.model("Product");
 
 request1 = request('http://localhost:3000');
 describe('GET /products or /prducts/:id', function(){
     product_new = null;
     beforeEach(function (done) {
-      ProductModel.remove().exec();
+      mockgoose.reset();
       product_new = new ProductModel({name:"test",product_id:2});
       product_new.save(done);
   });
@@ -21,14 +21,14 @@ describe('GET /products or /prducts/:id', function(){
         .set('Accept', 'application/json')
         .expect(200)
         .end(function(err,response){
-           if(err){
-              done(err);
-          }
-          var product = response.body[0]
-          product.should.have.property("name","test");
-          product.should.have.property("product_id","2");
-          done();
-      });
+         if(err){
+          done(err);
+      }
+      var product = response.body[0]
+      product.should.have.property("name","test");
+      product.should.have.property("product_id","2");
+      done();
+  });
     });
 
 
@@ -55,21 +55,32 @@ describe('GET /products or /prducts/:id', function(){
         .set('Accept', 'application/json')
         .expect(404,done);
     });
+    afterEach(function (done) {
+            mockgoose.reset();
+            done();
+        });
 });
 describe('GET /products failed', function(){
     beforeEach(function (done) {
         ProductModel.remove().exec(done);
     });
+
     it('should respond with 404 for get all products', function(done){
         request1
         .get('/products/')
         .expect(404,done);
     });
+
+    afterEach(function (done) {
+            mockgoose.reset();
+            done();
+        });
 });
 
 describe('POST /products successful', function(){
     beforeEach(function (done) {
-        ProductModel.remove().exec(done);
+        mockgoose.reset();
+        done();
     });
     it('should return 201 for post successfully', function(done){
         request1
@@ -78,6 +89,10 @@ describe('POST /products successful', function(){
             product_id:4})
         .expect(201,done);
     });
+    afterEach(function (done) {
+            mockgoose.reset();
+            done();
+        });
 });
 
 
