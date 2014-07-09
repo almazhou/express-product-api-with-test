756d9d17a26c9e668c98a8615592cd194462bc4b
almazhou <zx_new@126.com>
2014-07-09 09:06:38 +0800

    add check price id

---
+++ test/price_test.js
@@ -0,0 +1,36 @@
+var request = require('supertest'),
+should = require('should');
+var app = require("../server");
+var mongoose = require("mongoose");
+var mockgoose = require("mockgoose");
+mockgoose(mongoose);
+Product = mongoose.model("Product");
+Pricing = mongoose.model("Pricing");
+
+
+request = request("http://localhost:3000");
+describe("/GET all pricings",function(){
+	beforeEach(function (done) {
+      mockgoose.reset();
+      pricing = new Pricing({amount:45.0});
+      pricing.save();
+
+      product_new = new Product({name:"test",product_id:2});
+      product_new.save();
+
+      product_new.addPricing(pricing,done);
+
+
+  });
+
+	it("should return 200 when success",function(done){
+		request
+		.get("/products/"+product_new._id+"/pricings")
+		.expect(200,function(err,res){
+      var body = res.body;
+      body[0].should.equal(String(pricing._id));
+      done();
+    });
+	});
+
+});
